ffdc3e3370a24e8bbe1e8bab1be33e1f6254357b
João Ricardo Gonçalves da Silva <joaojuby@live.com>
2018-01-07 19:26:21 +0000

    Adicionada classe modeloteste

---
+++ VSHelloWorld_120221053/Models/ModeloTEste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSHelloWorld_120221053.Models
+{
+    public class ModeloTEste
+    {
+        public string nome;
+    }
+}
